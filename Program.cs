455a4068360696d0552b98e1f40d2a548db5593a
leeool <lgonsalez02@gmail.com>
2022-04-08 22:09:35 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Double a, b, c;
+
+Console.Write("\n(a): ");
+a = Convert.ToDouble(Console.ReadLine());
+ 
+Console.Write("(b): ");
+b = Convert.ToDouble(Console.ReadLine());
+ 
+Console.Write("(c): ");
+c = Convert.ToDouble(Console.ReadLine());
+
+double delta = Math.Pow(b, 2) - 4 * a * c;
+
+if (a == 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("\nNão é uma equação de segundo grau!\n");
+}
+
+else if (delta < 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"\nComo delta = {delta}, a equação não possui raízes reais!\n");
+}
+
+else
+{
+    double x1 = (-b + Math.Sqrt(delta)) / 2;
+    double x2 = (-b - Math.Sqrt(delta)) / 2;
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"\nX1 = {x1} e X2 = {x2}\n");
+}
+
+Console.ResetColor();
+
+
+
